fe48385390ce9e26326abe2bf1d804f4c74dc7a8
JHON JAIRO CARREÑO LOPEZ <95831198@comunidadort.edu.ar>
2021-04-07 22:12:41 -0300

    Resolucion ejercicio Beers

--- Ejercicios/beers.js
+++ Ejercicios/beers.js
@@ -13,9 +13,50 @@
   Beers
 */
 const beers = [
-    { name: 'Purple Iris', abv: 6.8, label: 'https://s3.amazonaws.com/brewerydbapi/beer/dMLwGo/upload_yiUllE-large.png', type: 'IPA' },
-    { name: 'Orange Blossom Pilsner', abv: 5.5, label: 'https://s3.amazonaws.com/brewerydbapi/beer/Rczcb9/upload_9Nhxxl-large.png', type: 'Pilsner' },
-    { name: 'Darkness', abv: 4.2, label: 'https://s3.amazonaws.com/brewerydbapi/beer/lnxbIV/upload_idNXFf-large.png', type: 'Stout' },
-    { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
-    { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
-  ];
+  { name: 'Purple Iris', abv: 6.8, label: 'https://s3.amazonaws.com/brewerydbapi/beer/dMLwGo/upload_yiUllE-large.png', type: 'IPA' },
+  { name: 'Orange Blossom Pilsner', abv: 5.5, label: 'https://s3.amazonaws.com/brewerydbapi/beer/Rczcb9/upload_9Nhxxl-large.png', type: 'Pilsner' },
+  { name: 'Darkness', abv: 4.2, label: 'https://s3.amazonaws.com/brewerydbapi/beer/lnxbIV/upload_idNXFf-large.png', type: 'Stout' },
+  { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
+  { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
+];
+
+
+function precio(beers, beerEnOferta) {
+
+  for (let i = 0; i < beers.length; i++) {
+
+    beers[i].file_name = ultimoElemento(beers[i].label);
+
+    if (beers[i].name === beerEnOferta) {
+      beers[i].precio = 320;
+    } else if (beers[i].abv < 5 && beers[i].name !== beerEnOferta) {
+      beers[i].precio = 300;
+    } else if (beers[i].abv >= 5 && beers[i].name !== beerEnOferta)
+      beers[i].precio = 350;
+  }
+
+  return beers;
+}
+
+
+function ultimoElemento(url) {
+
+  let nameLabe = url.split('/');
+
+  return nameLabe[nameLabe.length - 1];
+}
+
+function ordenar(beer) {
+
+  let orden = beer.sort(function (x, y) {
+    if (x.type < y.type) {
+      return -1;
+    }
+    return 1;
+  });
+
+  return orden;
+}
+const nuevoArreglo = precio(beers, 'Purple Iris');
+
+console.log(ordenar(nuevoArreglo));
