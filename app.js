8b7a0b8fad5a789e107e40e4d0c55aee0535ca51
unknown <pablof@tecnoshare.com>
2021-04-20 19:30:35 -0300

    subo npm ejemplo

---
+++ app.js
@@ -0,0 +1,6 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
+console.log(validator.isEmail('asdfasdf@tecnoshare.com'));
+
+console.log(chalk.blueBright.greenBright('Hola mundo!! en verde'));
